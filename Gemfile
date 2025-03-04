d0bc0b6e4674398f0ffbc05951d728d2da225108
Musaib03 <wanimusaib520@gmail.com>
2025-03-05 00:09:54 +0530

    Models upto 6

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem "jbuilder"
 
 gem 'bootstrap-sass', '~> 3.4.1'
 gem 'sassc-rails', '>= 2.1.0'
+gem "bcrypt",          "3.1.18"
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_25_175430) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_04_182737) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
