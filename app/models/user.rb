2e65d7620966e7c8a9756787fff3da67235d9727
Musaib03 <wanimusaib520@gmail.com>
2025-03-11 22:03:22 +0530

    site working but email and test showing some error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -25,9 +27,10 @@ class User < ApplicationRecord
     remember_digest
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
@@ -35,5 +38,16 @@ class User < ApplicationRecord
   end
   def session_token
     remember_digest || remember
+  end
+  private
+  # Converts email to all lowercase.
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,10 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: True)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
 
 
 
@@ -22,6 +25,8 @@ User.create!(name:  "Example User",
   password = "password"
   User.create!(name:  name,
                email: email,
-               password:              password,
-               password_confirmation: password)
+               password:password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
